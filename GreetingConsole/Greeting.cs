1d59c277b0dcde0d5d69d2e492fddecead711818
csgs104 <stefano98casagrande@gmail.com>
2023-05-19 11:39:50 +0200

    updates and changes

---
+++ GreetingConsole/Greeting.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+
+
+namespace GreetingConsole;
+
+public class Greeting
+{
+    public Greeting()
+    { }
+
+    public string Greet1(string name)
+    {
+        return $"Hello, {name}.";
+    }
+
+    public string Greet2(string? name = default)
+    {
+        return name is null
+            ? $"Hello, my friend."
+            : $"Hello, {name}.";
+    }
+
+    public string Greet3(string? name = default)
+    {
+        return name is null
+            ? $"Hello, my friend."
+            : (name == name.ToUpper())
+                ? $"HELLO {name}!"
+                : $"Hello, {name}.";
+    }
+
+    public string Greet4(params string[]? names)
+    {
+        return names is null
+            ? $"Hello, my friend."
+            : (names.Length > 1)
+                ? $"Hello, {names[0]} and {names[1]}."
+                : (names[0] == names[0].ToUpper())
+                    ? $"HELLO {names[0]}!"
+                    : $"Hello, {names[0]}.";
+    }
+
+    public string Greet5(params string[]? names)
+    {
+        return names is null
+            ? $"Hello, my friend."
+            : (names.Length == 2)
+                ? $"Hello, {names[0]} and {names[1]}."
+                : (names.Length > 2)
+                    ? $"Hello, {names.Take(names.Length - 1).Aggregate((p, s) => $"{p}, {s}")}, and {names[names.Length - 1]}."
+                    : (names[0] == names[0].ToUpper())
+                        ? $"HELLO {names[0]}!"
+                        : $"Hello, {names[0]}.";
+    }
+
+    public string Greet6(params string[]? names)
+    {
+        var greeting = new StringBuilder();
+
+        var except = new string[] { string.Empty };
+        var normal = GreetNormal(names?.Select(n => n != n.ToUpper() ? n : string.Empty).Except(except).ToArray());
+        var shout = GreetShout(names?.Select(n => n == n.ToUpper() ? n : string.Empty).Except(except).ToArray());
+
+        if (names is null) greeting.Append($"Hello, my friend.");
+        if (normal != string.Empty) greeting.Append(normal);
+        if (normal != string.Empty && shout != string.Empty) greeting.Append(" AND ");
+        if (shout != string.Empty) greeting.Append(shout);
+
+        return greeting.ToString();
+    }
+
+    public string Greet7(params string[]? names)
+    {
+        var greeting = new StringBuilder();
+
+        var list = names?.Aggregate((p, s) => $"{p}, {s}").Split(',', StringSplitOptions.TrimEntries);
+        var normal = GreetNormal(list?.Select(n => n != n.ToUpper() ? n : string.Empty).Except(new string[] { string.Empty }).ToArray());
+        var shout = GreetShout(list?.Select(n => n == n.ToUpper() ? n : string.Empty).Except(new string[] { string.Empty }).ToArray());
+
+        if (list is null) greeting.Append($"Hello, my friend.");
+        if (normal != string.Empty) greeting.Append(normal);
+        if (normal != string.Empty && shout != string.Empty) greeting.Append(" AND ");
+        if (shout != string.Empty) greeting.Append(shout);
+
+        return greeting.ToString();
+    }
+
+    public string Greet8(params string[]? names)
+    {
+        var greeting = new StringBuilder();
+
+        var normal = GreetNormal(Builder(shout: false, names: names));
+        var shout = GreetShout(Builder(shout: true, names: names));
+
+        if (names is null) greeting.Append($"Hello, my friend.");
+        if (normal is not null && normal != string.Empty) greeting.Append(normal);
+        if (normal is not null && normal != string.Empty && shout is not null && shout != string.Empty) greeting.Append(" AND ");
+        if (shout is not null && shout != string.Empty) greeting.Append(shout);
+
+        return greeting.ToString();
+    }
+
+    private string[]? Builder(bool shout, char sep = ',', char specialsep = '"', params string[]? names)
+    {
+        var namesDic = names?.ToDictionary(n => n, n =>
+                                        (isShout: n == n.ToUpper(),
+                                         isMore: n.Contains(specialsep),
+                                         isSpecial: n != n.TrimStart(specialsep) && n != n.TrimEnd(specialsep) && n != n.Trim(specialsep)));
+
+        return namesDic?.Select(n => shout == n.Value.isShout ? n.Value.isSpecial
+                                                       ? n.Key.Trim(specialsep)
+                                                       : n.Value.isMore
+                                                       ? n.Key.Split(sep, StringSplitOptions.TrimEntries).Aggregate((p, s) => $"{p}{Environment.NewLine}{s}")
+                                                       : n.Key
+                                                       : string.Empty)
+                                          .Aggregate((p, s) => $"{p}{Environment.NewLine}{s}")
+                                          .Split(Environment.NewLine, StringSplitOptions.TrimEntries)
+                                          .Except(new string[] { string.Empty })
+                                          .ToArray();
+    }
+
+    private string GreetNormal(params string[]? names)
+    {
+        return names is null
+            ? string.Empty
+            : (names.Length == 0)
+            ? string.Empty
+            : (names.Length == 1)
+            ? $"Hello, {names[0]}."
+            : (names.Length == 2)
+            ? $"Hello, {names[0]} and {names[1]}."
+            : (names.Length > 2)
+            ? $"Hello, {names.Take(names.Length - 1).Aggregate((p, s) => $"{p}, {s}")}, and {names[names.Length - 1]}."
+            : string.Empty;
+    }
+
+    private string GreetShout(params string[]? names)
+    {
+        return names is null
+            ? string.Empty
+            : (names.Length == 0)
+            ? string.Empty
+            : (names.Length == 1)
+            ? $"HELLO {names[0]}!"
+            : (names.Length == 2)
+            ? $"HELLO {names[0]} AND {names[1]}!"
+            : (names.Length > 2)
+            ? $"HELLO {names.Take(names.Length - 1).Aggregate((p, s) => $"{p} AND {s}")} AND {names[names.Length - 1]}!"
+            : string.Empty;
+    }
+
+    public string Greet(params string[]? namesList)
+    {
+        var str = new StringBuilder();
+        if (namesList is null)
+        {
+            str.Append("Hello, my friend.");
+        }
+        else
+        {
+            var names = new List<string>();
+            var greeted = new List<string>();
+            var shouted = new List<string>();
+
+            foreach (var nameList in namesList)
+            {
+                if (nameList is null)
+                {
+                    names.Add("my friend");
+                }
+                else
+                {
+                    if (nameList != nameList.TrimStart('"') && 
+			            nameList != nameList.TrimEnd('"') && 
+			            nameList != nameList.Trim('"'))
+                    {
+                        names.Add(nameList.Trim('"'));
+                    }
+                    else
+                    {
+                        foreach (var name in nameList.Split(","))
+                        {
+                            names.Add(name.Trim());
+                        }
+                    }
+                }
+            }
+
+            foreach (var name in names)
+            {
+                if (name != name.ToUpper())
+                {
+                    greeted.Add(name);
+                }
+                else
+                {
+                    shouted.Add(name);
+                }
+            }
+
+            if (greeted.Count >= 1)
+            {
+                str.Append("Hello, ");
+            }
+            if (greeted.Count == 1)
+            {
+                str.Append(greeted[0]);
+                str.Append(".");
+            }
+            if (greeted.Count == 2)
+            {
+                str.Append(greeted[0]);
+                str.Append(" and ");
+                str.Append(greeted[1]);
+                str.Append(".");
+            }
+            if (greeted.Count > 2)
+            {
+                foreach (var name in greeted.SkipLast(1))
+                {
+                    str.Append(name);
+                    str.Append(", ");
+                }
+                str.Append("and ");
+                str.Append(greeted.Last());
+                str.Append(".");
+            }
+
+            if (greeted.Count >= 1 && shouted.Count >= 1) 
+	        {
+                str.Append(" AND ");
+            }
+
+            if (shouted.Count >= 1)
+            {
+                str.Append("HELLO ");
+            }
+            if (shouted.Count == 1)
+            {
+                str.Append(shouted[0]);
+                str.Append("!");
+            }
+            if (shouted.Count == 2)
+            {
+                str.Append(shouted[0]);
+                str.Append(" AND ");
+                str.Append(shouted[1]);
+                str.Append("!");
+            }
+            if (shouted.Count > 2)
+            {
+                foreach (var name in shouted.SkipLast(1))
+                {
+                    str.Append(name);
+                    str.Append(", ");
+                }
+                str.Append("AND ");
+                str.Append(shouted.Last());
+                str.Append("!");
+            }
+        }
+        return str.ToString();
+    }
+}
---
+++ GreetingTest/GreetingTest.cs
@@ -0,0 +1,134 @@
+﻿using GreetingConsole;
+using Xunit.Abstractions;
+
+
+namespace GreetingTest;
+
+public class GreetingTest
+{
+    private readonly Greeting _sut;
+    private readonly ITestOutputHelper _output;
+
+
+    public GreetingTest(ITestOutputHelper output)
+    {
+        _sut = new Greeting();
+        _output = output;
+    }
+
+
+    [Fact]
+    public void SimpleGreet()
+    {
+        Assert.True(true);
+    }
+
+    [Theory] // called 3 times
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(6)]
+    public void TestTheory(int number) 
+    {
+        Assert.False(number % 2 == 1);
+    }
+
+    [Fact]
+    public void Should_Greet1()
+    {
+        Assert.Equal("Hello, Bob.", _sut.Greet1("Bob"));
+    }
+
+    [Fact]
+    public void Should_Greet2()
+    {
+        Assert.Equal("Hello, Bob.", _sut.Greet2("Bob"));
+        Assert.Equal("Hello, my friend.", _sut.Greet2(null));
+    }
+
+    [Fact]
+    public void Should_Greet3()
+    {
+        Assert.Equal("Hello, Bob.", _sut.Greet3("Bob"));
+        Assert.Equal("Hello, my friend.", _sut.Greet3(null));
+        Assert.Equal("HELLO JERRY!", _sut.Greet3("JERRY"));
+    }
+
+    [Fact]
+    public void Should_Greet4()
+    {
+        Assert.Equal("Hello, Bob.", _sut.Greet4("Bob"));
+        Assert.Equal("Hello, my friend.", _sut.Greet4(null));
+        Assert.Equal("HELLO JERRY!", _sut.Greet4("JERRY"));
+        Assert.Equal("Hello, Jill and Jane!", _sut.Greet4("Jill", "Jane"));
+    }
+
+    [Fact]
+    public void Should_Greet5()
+    {
+        Assert.Equal("Hello, Bob.", _sut.Greet5("Bob"));
+        Assert.Equal("Hello, my friend.", _sut.Greet5(null));
+        Assert.Equal("HELLO JERRY!", _sut.Greet5("JERRY"));
+        Assert.Equal("Hello, Jill and Jane.", _sut.Greet5("Jill", "Jane"));
+        Assert.Equal("Hello, Amy, Brian, and Charlotte.", _sut.Greet5("Amy", "Brian", "Charlotte"));
+    }
+
+    [Fact]
+    public void Should_Greet6()
+    {
+        Assert.Equal("Hello, Bob.", _sut.Greet6("Bob"));
+        Assert.Equal("Hello, my friend.", _sut.Greet6(null));
+        Assert.Equal("HELLO JERRY!", _sut.Greet6("JERRY"));
+        Assert.Equal("Hello, Jill and Jane.", _sut.Greet6("Jill", "Jane"));
+        Assert.Equal("Hello, Amy, Brian, and Charlotte.", _sut.Greet6("Amy", "Brian", "Charlotte"));
+        Assert.Equal("Hello, Amy and Charlotte. AND HELLO BRIAN!", _sut.Greet6("Amy", "BRIAN", "Charlotte"));
+        Assert.Equal("Hello, Amy, Jill, and Charlotte. AND HELLO BRIAN!", _sut.Greet6("Amy", "Jill", "BRIAN", "Charlotte"));
+    }
+
+    [Fact]
+    public void Should_Greet7()
+    {
+        Assert.Equal("Hello, Bob.", _sut.Greet7("Bob"));
+        Assert.Equal("Hello, my friend.", _sut.Greet7(null));
+        Assert.Equal("HELLO JERRY!", _sut.Greet7("JERRY"));
+        Assert.Equal("Hello, Jill and Jane.", _sut.Greet7("Jill", "Jane"));
+        Assert.Equal("Hello, Amy, Brian, and Charlotte.", _sut.Greet7("Amy", "Brian", "Charlotte"));
+        Assert.Equal("Hello, Amy and Charlotte. AND HELLO BRIAN!", _sut.Greet7("Amy", "BRIAN", "Charlotte"));
+        Assert.Equal("Hello, Amy, Jill, and Charlotte. AND HELLO BRIAN!", _sut.Greet7("Amy", "Jill", "BRIAN", "Charlotte"));
+        Assert.Equal("Hello, Amy, Jill, Jim, and Charlotte. AND HELLO BRIAN!", _sut.Greet7("Amy", "Jill, Jim", "BRIAN", "Charlotte"));
+    }
+
+    [Fact]
+    public void Should_Greet8()
+    {
+        Assert.Equal("Hello, Bob.", _sut.Greet8("Bob"));
+        Assert.Equal("Hello, my friend.", _sut.Greet8(null));
+        Assert.Equal("HELLO JERRY!", _sut.Greet8("JERRY"));
+        Assert.Equal("Hello, Jill and Jane.", _sut.Greet8("Jill", "Jane"));
+        Assert.Equal("Hello, Amy, Brian, and Charlotte.", _sut.Greet8("Amy", "Brian", "Charlotte"));
+        Assert.Equal("Hello, Amy and Charlotte. AND HELLO BRIAN!", _sut.Greet8("Amy", "BRIAN", "Charlotte"));
+        Assert.Equal("Hello, Amy, Jill, and Charlotte. AND HELLO BRIAN!", _sut.Greet8("Amy", "Jill", "BRIAN", "Charlotte"));
+        Assert.Equal("Hello, Amy, Jill, Jim, and Charlotte. AND HELLO BRIAN!", _sut.Greet8("Amy", "Jill, Jim", "BRIAN", "Charlotte"));
+        Assert.Equal("Hello, Amy, Jill, Jim, and Charlotte, Michael. AND HELLO BRIAN!", _sut.Greet8("Amy", "Jill, Jim", "BRIAN", "\"Charlotte, Michael\""));
+    }
+
+    [Fact]
+    public void Should_Greet()
+    {
+        Assert.True(true);
+        Assert.Equal("Hello, Bob.", _sut.Greet("Bob"));
+        Assert.Equal("Hello, my friend.", _sut.Greet(null));
+        Assert.Equal("HELLO JERRY!", _sut.Greet("JERRY"));
+        Assert.Equal("Hello, Jill and Jane.", _sut.Greet("Jill", "Jane"));
+        Assert.Equal("Hello, Amy, Brian, and Charlotte.", _sut.Greet("Amy", "Brian", "Charlotte"));
+        Assert.Equal("Hello, Amy and Charlotte. AND HELLO BRIAN!", _sut.Greet("Amy", "BRIAN", "Charlotte"));
+        Assert.Equal("Hello, Amy, Jill, and Charlotte. AND HELLO BRIAN!", _sut.Greet("Amy", "Jill", "BRIAN", "Charlotte"));
+        Assert.Equal("Hello, Bob, Charlie, and Dianne.", _sut.Greet("Bob", "Charlie, Dianne"));
+        Assert.Equal("Hello, Bob and Charlie, Dianne.", _sut.Greet("Bob", "\"Charlie, Dianne\""));
+    }
+
+    [Fact(Skip = "Sandbox")]
+    public void SandBox() 
+    {
+        _output.WriteLine(_sut.Greet("Pippo"));
+    }
+}
